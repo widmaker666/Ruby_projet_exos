7422457c108ace49a1156630b727ed717ba4645e
widmaker666 <114477330+widmaker666@users.noreply.github.com>
2022-10-03 15:59:25 +0200

    Add files via upload

---
+++ exo_08.rb
@@ -0,0 +1,9 @@
+puts "Re, t'as rien à faire que jouer ?"
+puts "vas y go alors"
+puts "Choisis bien ton nombre et regarde la magie"
+chiffre = gets.chomp.to_i
+i = 0
+while i != chiffre
+    chiffre = chiffre - 1
+    puts chiffre
+end    
