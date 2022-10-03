e5ed2fc34e495b1abe41e5eec4617d4526bb5e8b
widmaker666 <114477330+widmaker666@users.noreply.github.com>
2022-10-03 15:22:35 +0200

    Add files via upload

---
+++ exo_06.rb
@@ -0,0 +1,5 @@
+puts "Salut toi, j'ai encore un autre jeu débile !"
+puts "est ce que t'es chaud(e) ?"
+puts "ecris un nombre ou un chiffre et ensuite tu prend une pose de bad gone !"
+chiffre = gets.chomp.to_i
+puts "#{'Hello you !' * (chiffre-1)}"
