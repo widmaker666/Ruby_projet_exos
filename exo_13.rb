1e5d824e9b5f06e02f72623a12de7974e283ff24
widmaker666 <114477330+widmaker666@users.noreply.github.com>
2022-10-03 19:58:50 +0200

    Add files via upload

---
+++ exo_13.rb
@@ -0,0 +1,14 @@
+tableau = [] 
+for num in 1..50 do
+    if num < 10 
+        tableau = tableau + ["jean.dupont.0#{num}@email.fr"]
+    else 
+        tableau = tableau + ["jean.dupont.#{num}@email.fr"]
+    end   
+    
+end  
+
+puts tableau.join('/') 
+
+    
+ 
