fbad73350bb3b7efda785c5903601db77780d389
widmaker666 <114477330+widmaker666@users.noreply.github.com>
2022-10-03 14:49:46 +0200

    Add files via upload

---
+++ exo_05.rb
@@ -0,0 +1,6 @@
+puts "Salut toi, j'ai encore un autre jeu débile !"
+puts "est ce que t'es chaud(e) ?"
+puts "ecris un nombre ou un chiffre et ensuite tu prend une pose de bad gone !"
+chiffre = gets.chomp.to_i
+puts "#{'salut, ca fart ?' * chiffre}"
+
