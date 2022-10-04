c93d0a5a8bf7714b6468fca4e3bc1da8de5422e6
widmaker666 <114477330+widmaker666@users.noreply.github.com>
2022-10-04 10:29:39 +0200

    Add files via upload

--- exo_03.rb
+++ exo_03.rb
@@ -1,5 +1,5 @@
 puts "Bonjour cher utilisateur lambda"
 puts "quelle est ta date de naissance ?"
 year = gets.chomp.to_i
-puts "Voici ton âge en 2022"
-puts "#{2022-year} ans ou presque selon si tu es de fin ou début d'année... T'es vieux ! :)" 
+puts "Voici ton âge en 2017"
+puts "#{2017-year} ans ou presque selon si tu es de fin ou début d'année... T'es vieux ! :)" 
